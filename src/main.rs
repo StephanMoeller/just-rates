ba331ef8db803505407c3d0825661989fbcf4048
Stephan Møller <stephanryer@hotmail.com>
2023-01-30 00:01:06 +0100

    work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 
+use std::net::{UdpSocket, TcpListener};
 use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
 
@@ -12,6 +13,23 @@ fn main() -> std::io::Result<()> {
     let udp_addr = "127.0.0.1:8343";
     let tcp_addr = "127.0.0.1:8081";
 
+    let (udp_socket, tcp_listener) = setup_udp_socket_and_tcp_listener(udp_addr, tcp_addr);
+    let publisher_thread = std::thread::spawn(move || {
+        app::create_publish_listener(udp_socket, data_message_sender).unwrap();
+    });
+
+    let consumer_thread = std::thread::spawn(move || {       
+        app::create_consumer_endpoint(tcp_listener, data_message_receiver);
+    });
+    
+    publisher_thread.join().unwrap();
+    consumer_thread.join().unwrap();
+
+    return Ok(());
+}
+
+fn setup_udp_socket_and_tcp_listener(udp_addr: &str, tcp_addr: &str) -> (UdpSocket, TcpListener){
+
     // Create local udp and tcp stuff in main thread before moving to threads to break early on main thread if any of these two fails
     let local_socket = match std::net::UdpSocket::bind(&String::from(udp_addr)) {
         Ok(socket) => socket,
@@ -23,16 +41,5 @@ fn main() -> std::io::Result<()> {
         Err(msg) => panic!("Error binding tcp listener on local address {tcp_addr}: {msg}")
     };
 
-    let publisher_thread = std::thread::spawn(move || {
-        app::create_publish_listener(local_socket, data_message_sender).unwrap();
-    });
-
-    let consumer_thread = std::thread::spawn(move || {       
-        app::create_consumer_endpoint(local_tcp_listener, data_message_receiver);
-    });
-    
-    publisher_thread.join().unwrap();
-    consumer_thread.join().unwrap();
-
-    return Ok(());
+    return (local_socket, local_tcp_listener);
 }

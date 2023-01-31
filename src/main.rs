a39d4e58c3b594bd431541dcd12d868b8cf27fe6
Stephan Møller <stephanryer@hotmail.com>
2023-01-31 16:50:49 +0100

    refactorings

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,6 @@ fn main() -> std::io::Result<()> {
     let publisher_udp_port: i32 = 11311;
     let websocket_tcp_port: i32 = 8081;
 
-    // POC udp message spammer
-    let client = UdpSocket::bind("127.0.0.1:0").unwrap();
-    let server_addr = format!("127.0.0.1:{publisher_udp_port}");
-    std::thread::spawn(move || {
-        let mut counter: i32 = 0;
-
-        loop {
-            counter += 1;
-            client
-                .send_to(
-                    format!("DATA This is data message number {counter}").as_bytes(),
-                    &server_addr,
-                )
-                .unwrap();
-            std::thread::sleep(std::time::Duration::from_secs(1));
-        }
-    });
-
     // Serve static html file on seperate port
     let tcp_listener = TcpListener::bind("127.0.0.1:8082").unwrap();
 
@@ -46,6 +28,27 @@ fn main() -> std::io::Result<()> {
     // Actual application
     app::run(publisher_udp_port, websocket_tcp_port).unwrap();
     return Ok(());
+}
+
+fn _start_client_spammer(publisher_udp_port: i32)
+{
+    // POC udp message spammer
+    let client = UdpSocket::bind("127.0.0.1:0").unwrap();
+    let server_addr = format!("127.0.0.1:{publisher_udp_port}");
+    std::thread::spawn(move || {
+        let mut counter: i32 = 0;
+
+        loop {
+            counter += 1;
+            client
+                .send_to(
+                    format!("DATA This is data message number {counter}").as_bytes(),
+                    &server_addr,
+                )
+                .unwrap();
+            std::thread::sleep(std::time::Duration::from_secs(1));
+        }
+    });
 }
 
 fn create_http_response(filename: &str) -> String {
